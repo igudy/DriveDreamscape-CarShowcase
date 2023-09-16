5c2bae09409a96d6374fb593aa297901c83380a3
igudy <goodnessigunma1@gmail.com>
2023-09-17 00:09:50 +0100

    asessts added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import React from "react";
 import { CustomButton } from ".";
 
 const Hero = () => {
+  const handleScroll = () => {};
+
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
@@ -14,7 +18,11 @@ const Hero = () => {
           process.
         </p>
 
-        <CustomButton />
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
       </div>
     </div>
   );
