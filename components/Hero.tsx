cde977f86a8d82e92e26c950c839339619b69a26
igudy <goodnessigunma1@gmail.com>
2023-09-17 20:20:43 +0100

    hero section implemented

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { CustomButton } from ".";
+import Image from "next/image";
 
 const Hero = () => {
   const handleScroll = () => {};
@@ -23,6 +24,14 @@ const Hero = () => {
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          {/* object contain so it won't lose it aspect ratio */}
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+        </div>
+
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
